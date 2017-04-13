561a59fce66c981644f6bf38e0dce96cbb4a160a
abe@human.ait.kyushu-u.ac.jp <abe@human.ait.kyushu-u.ac.jp>
2017-04-13 22:05:18 +0900

    convert as maximum

--- ttfotf2png.py
+++ ttfotf2png.py
@@ -27,25 +27,26 @@ def convert_binary_img(pil_img, threshold=128):
     binary_img = num2pil(num_img)
     return binary_img
 
-def get_offset(pil_img):
+def get_offset(pil_img, normal_canvas_size):
     num_img = pil2num(pil_img)
     canvas_size = len(num_img)
-    margins = {'top':-1, 'right':-1, 'bottom':-1, 'left':-1}
+    canvas_offset = canvas_size - normal_canvas_size
+    margins = {}
     # top
     for i in range(canvas_size):
         for j in range(canvas_size):
             if num_img[i][j] != 255:
                 margins['top'] = i
                 break
-        if margins['top'] != -1:
+        if 'top' in margins:
             break
     # bottom
     for i in range(canvas_size):
         for j in range(canvas_size):
             if num_img[canvas_size - i - 1][j] != 255:
-                margins['bottom'] = i
+                margins['bottom'] = i - canvas_offset
                 break
-        if margins['bottom'] != -1:
+        if 'bottom' in margins:
             break
     # left
     for j in range(canvas_size):
@@ -53,42 +54,55 @@ def get_offset(pil_img):
             if num_img[i][j] != 255:
                 margins['left'] = j
                 break
-        if margins['left'] != -1:
+        if 'left' in margins:
             break
     # right
     for j in range(canvas_size):
         for i in range(canvas_size):
             if num_img[i][canvas_size - j - 1] != 255:
-                margins['right'] = j
+                margins['right'] = j - canvas_offset
                 break
-        if margins['right'] != -1:
+        if 'right' in margins:
             break
     x_offset = int((margins['right'] - margins['left']) / 2)
     y_offset = int((margins['bottom'] - margins['top']) / 2)
-    return x_offset, y_offset
+    offsets = (x_offset, y_offset)
+    # print (margins)
 
-def draw_char(char, font, canvas_size, x_offset=0, y_offset=0):
+    is_tb_maximum = margins['top'] + margins['bottom'] <= 0
+    is_lr_maximum = margins['right'] + margins['left'] <= 0
+    is_maximum = is_tb_maximum or is_lr_maximum
+    return offsets, is_maximum
+
+def draw_char(char, font_path, canvas_size, char_size, offsets=(0, 0)):
+    font = ImageFont.truetype(font_path, size=char_size)
     img = Image.new('L', (canvas_size, canvas_size), 255)
     draw = ImageDraw.Draw(img)
-    draw.text((x_offset, y_offset), char, 0, font=font)
+    draw.text(offsets, char, 0, font=font)
     img = convert_binary_img(img)
     return img
 
-def draw_char_center(char, font, canvas_size):
-    no_offset_img = draw_char(char, font, canvas_size)
-    x_offset, y_offset = get_offset(no_offset_img)
-    center_img = draw_char(char, font, canvas_size, x_offset, y_offset)
-    return center_img
+def draw_char_center(char, font_path, canvas_size, char_size):
+    no_offset_img = draw_char(char, font_path, canvas_size + 20, char_size)
+    offsets, is_maximum = get_offset(no_offset_img, canvas_size)
+    center_img = draw_char(char, font_path, canvas_size, char_size, offsets)
+    return center_img, is_maximum
 
-def ttfotf2png(src_font, dst_dir, charset, char_size, canvas_size):
-    src_font = ImageFont.truetype(src_font, size=char_size)
+def draw_char_maximum(char, font_path, canvas_size):
+    char_size = canvas_size
+    while True:
+        img, is_maximum = draw_char_center(char, font_path, canvas_size, char_size)
+        if is_maximum:
+            break
+        char_size += 1
+    return img
 
+def ttfotf2png(src_font_path, dst_dir_path, charset, canvas_size):
     for c in CAPS:
-        img = draw_char_center(c, src_font, canvas_size)
-        img.point(lambda x: x * 0.5)
+        img = draw_char_maximum(c, src_font_path, canvas_size)
         if img:
-            img.save(os.path.join(dst_dir, c + ".png"))
+            img.save(os.path.join(dst_dir_path, c + ".png"))
             print ("proccessed: " + c)
 
 if __name__ == "__main__":
-    ttfotf2png("./azukiLP.ttf", "output", CAPS, 100, 100)
+    ttfotf2png("./azukiLP.ttf", "output", CAPS, 100)

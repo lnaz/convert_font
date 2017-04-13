75f1de10acb5e6759fdc96950d1af399b3c6aad3
abe@human.ait.kyushu-u.ac.jp <abe@human.ait.kyushu-u.ac.jp>
2017-04-13 20:58:51 +0900

    centering

--- ttfotf2png.py
+++ ttfotf2png.py
@@ -27,18 +27,64 @@ def convert_binary_img(pil_img, threshold=128):
     binary_img = num2pil(num_img)
     return binary_img
 
-def draw_char(char, font, canvas_size):
+def get_offset(pil_img):
+    num_img = pil2num(pil_img)
+    canvas_size = len(num_img)
+    margins = {'top':-1, 'right':-1, 'bottom':-1, 'left':-1}
+    # top
+    for i in range(canvas_size):
+        for j in range(canvas_size):
+            if num_img[i][j] != 255:
+                margins['top'] = i
+                break
+        if margins['top'] != -1:
+            break
+    # bottom
+    for i in range(canvas_size):
+        for j in range(canvas_size):
+            if num_img[canvas_size - i - 1][j] != 255:
+                margins['bottom'] = i
+                break
+        if margins['bottom'] != -1:
+            break
+    # left
+    for j in range(canvas_size):
+        for i in range(canvas_size):
+            if num_img[i][j] != 255:
+                margins['left'] = j
+                break
+        if margins['left'] != -1:
+            break
+    # right
+    for j in range(canvas_size):
+        for i in range(canvas_size):
+            if num_img[i][canvas_size - j - 1] != 255:
+                margins['right'] = j
+                break
+        if margins['right'] != -1:
+            break
+    x_offset = int((margins['right'] - margins['left']) / 2)
+    y_offset = int((margins['bottom'] - margins['top']) / 2)
+    return x_offset, y_offset
+
+def draw_char(char, font, canvas_size, x_offset=0, y_offset=0):
     img = Image.new('L', (canvas_size, canvas_size), 255)
     draw = ImageDraw.Draw(img)
-    draw.text((0, 0), char, 0, font=font)
+    draw.text((x_offset, y_offset), char, 0, font=font)
     img = convert_binary_img(img)
     return img
+
+def draw_char_center(char, font, canvas_size):
+    no_offset_img = draw_char(char, font, canvas_size)
+    x_offset, y_offset = get_offset(no_offset_img)
+    center_img = draw_char(char, font, canvas_size, x_offset, y_offset)
+    return center_img
 
 def ttfotf2png(src_font, dst_dir, charset, char_size, canvas_size):
     src_font = ImageFont.truetype(src_font, size=char_size)
 
     for c in CAPS:
-        img = draw_char(c, src_font, canvas_size)
+        img = draw_char_center(c, src_font, canvas_size)
         img.point(lambda x: x * 0.5)
         if img:
             img.save(os.path.join(dst_dir, c + ".png"))
